9f19db5c4146b2bb8ea73b6a1d09e4980491999b
Erick Gabriel dos Santos Alves <erickgabrielalves@hotmail.com>
2020-09-11 22:07:37 -0300

    CSS Quiz Page

--- script.js
+++ script.js
@@ -11,26 +11,30 @@ async function mostrarPergunta() {
     data.forEach((perguntaAtual, order) => {
         const options = [];
 
-        for(resposta in perguntaAtual.options) {
+        for (resposta in perguntaAtual.options) {
             options.push(
                 `
-                    <label>
-                        <input type="radio" name="${order}" value="${resposta}"></input>
-                        ${resposta} - ${perguntaAtual.options[resposta]}
-                    </label>
-                    </br>
+                    <div class="options">
+                        <label>
+                            <input type="radio" name="${order}" value="${resposta}"></input>
+                            ${resposta} - ${perguntaAtual.options[resposta]}
+                        </label>
+                        </br> 
+                    </div>
                 `
             );
         }
 
         pergResp.push(
             `
-                <div class="questions">${perguntaAtual.question}</div>
-                <div class="answer">${options.join('')}</div>
+                <div class="quiz">
+                    <div class="questions">${perguntaAtual.question}</div>
+                    <div class="answer">${options.join('')}</div>
+                </div>
             `
         );
     });
-    
+
     content.innerHTML = pergResp.join('');
 }
 mostrarPergunta()
