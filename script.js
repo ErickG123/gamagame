78234ff7737b4bf48e73e36aa50ca1c31b670ace
Erick Gabriel dos Santos Alves <erickgabrielalves@hotmail.com>
2020-09-14 09:44:58 -0300

    Atualizando lógica

--- script.js
+++ script.js
@@ -1,13 +1,40 @@
 const content = document.getElementById("questions");
+const buttons = document.getElementById("buttons");
 
-async function mostrarPergunta() {
+async function mostrarPergunta(queryString) {
     const response = await fetch("questions.json");
     const data = await response.json();
+    const params = new URLSearchParams(queryString);
 
-    console.log(data);
+    const currentQuestion = params.get("currentQuestion");
 
     const pergResp = [];
 
+    if (currentQuestion > data.length) {
+        /*TODO: Mostrar resultado do questionário*/
+    } else {
+        const perguntaEscolhida = data[currentQuestion - 1];
+
+        const options = perguntaEscolhida.options;
+        Object.entries(options).forEach(([order, resposta]) => {
+            const printedOption =    
+                `
+                    <div class="options">
+                        <label>
+                            <input type="radio" name="${order}" value="${resposta}"></input>
+                            ${order} - ${resposta}
+                        </label>
+                        </br>
+                    </div>
+                `
+            content.innerHTML += printedOption;
+            console.log(printedOption);
+        });
+
+
+    }
+
+    /*
     data.forEach((perguntaAtual, order) => {
         const options = [];
 
@@ -34,7 +61,9 @@ async function mostrarPergunta() {
             `
         );
     });
-
-    content.innerHTML = pergResp.join('');
+    */
 }
-mostrarPergunta()
+
+const queryString = window.location.search;
+
+mostrarPergunta(queryString)
